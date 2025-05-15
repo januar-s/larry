53c0cbbb2e219bbbfb12e15ab1c3c72e81872bc7
januar-s <jan.schmidt@stud.fhgr.ch>
2025-05-15 11:21:54 +0200

    fixed plus and minus

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,12 @@ document.addEventListener("DOMContentLoaded", () => {
     dropdowns.forEach(({ buttonId, contentId }) => {
         const button = document.getElementById(buttonId);
         const dropdown = document.getElementById(contentId);
+        const verticalLine = button.querySelector(".vertical");
 
         button.addEventListener("click", () => {
-            dropdown.classList.toggle("show");
-
-            // Swap icon
-            const isOpen = dropdown.classList.contains("show");
+            const isOpen = dropdown.classList.toggle("show");
             button.classList.toggle("rotated", isOpen);
-            button.src = isOpen ? "icons/minus.svg" : "icons/plus.svg";
-            button.alt = isOpen ? "minus" : "plus";
+            verticalLine.style.display = isOpen ? "none" : "block"; // hide vertical to show minus
         });
     });
 });
